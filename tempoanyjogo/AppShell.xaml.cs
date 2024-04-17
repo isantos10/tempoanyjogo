0fb028b54ce97c993f8b600f79a3b0dc049b9ce4
anygabrieli <isasantos0914@gmail.com>
2024-04-17 10:36:54 -0300

    any 17/04

---
+++ tempoanyjogo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace tempoanyjogo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ tempoanyjogo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace tempoanyjogo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
